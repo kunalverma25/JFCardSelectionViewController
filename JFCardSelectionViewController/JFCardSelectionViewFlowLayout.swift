61ea3e446e92bddea6d3342df195c951dcc24dea
Kunal Verma <kunal-verma@live.in>
2016-11-29 12:48:50 +0530

    Update JFCardSelectionViewFlowLayout.swift

--- JFCardSelectionViewController/JFCardSelectionViewFlowLayout.swift
+++ JFCardSelectionViewController/JFCardSelectionViewFlowLayout.swift
@@ -1,5 +1,5 @@
 /*
-* JFFocusedCardView
+* JFCardSelectionViewFlowLayout
 *
 * Created by Jeremy Fox on 3/1/16.
 * Copyright (c) 2016 Jeremy Fox. All rights reserved.
@@ -25,86 +25,25 @@
 
 import UIKit
 
-protocol JFFocusedCardViewDelegate {
-    func focusedCardViewDidSelectDetailAction(_ focusedCardView: JFFocusedCardView) -> Void
-    func focusedCardViewDidSelectActionItemOne(_ focusedCardView: JFFocusedCardView) -> Void
-    func focusedCardViewDidSelectActionItemTwo(_ focusedCardView: JFFocusedCardView) -> Void
+class JFCardSelectionViewFlowLayout: UICollectionViewFlowLayout {
+
+    override init() {
+        super.init()
+        scrollDirection = .horizontal
+        minimumLineSpacing = -10
+        let w = UIScreen.main.bounds.width / 4
+        let h = UIScreen.main.bounds.height / 6
+        itemSize = CGSize(width: w, height: h)
+        minimumInteritemSpacing = 1000
+        let midX = UIScreen.main.bounds.midX
+        let halfItemWidth = itemSize.width / 2
+        let left = midX - halfItemWidth
+        let right = left
+        sectionInset = UIEdgeInsets(top: 0, left: left, bottom: 0, right: right)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
-
-class JFFocusedCardView: UIView {
-
-    var card: CardPresentable!
-    var delegate: JFFocusedCardViewDelegate?
-    fileprivate var recognizer: UITapGestureRecognizer!
-    @IBOutlet var imageView: UIImageView!
-    @IBOutlet var titleLabel: UILabel!
-    @IBOutlet var subTitleLabelOne: UILabel!
-    @IBOutlet var subTitleLabelTwo: UILabel!
-    @IBOutlet weak var actionOneButton: UIButton!
-    @IBOutlet weak var actionTwoButton: UIButton!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        imageView.clipsToBounds = true
-        imageView.layer.cornerRadius = 2
-        imageView.layer.borderColor = UIColor.white.withAlphaComponent(0.4).cgColor
-        imageView.layer.borderWidth = 0.5
-        actionOneButton.isHidden = true
-        actionOneButton.layer.cornerRadius = 2
-        actionOneButton.layer.borderColor = UIColor.white.withAlphaComponent(0.5).cgColor
-        actionOneButton.layer.borderWidth = 0.5
-        actionTwoButton.isHidden = true
-        actionTwoButton.layer.cornerRadius = 2
-        actionTwoButton.layer.borderColor = UIColor.white.withAlphaComponent(0.5).cgColor
-        actionTwoButton.layer.borderWidth = 0.5
-        recognizer = UITapGestureRecognizer(target: self, action: #selector(tapAction))
-        imageView.addGestureRecognizer(recognizer)
-        imageView.isUserInteractionEnabled = true
-    }
-    
-    func configureForCard(_ card: CardPresentable?) {
-        guard let _card = card else {
-            self.card = nil
-            self.imageView.image = nil
-            self.titleLabel.text = nil
-            self.subTitleLabelOne.text = nil
-            self.subTitleLabelTwo.text = nil
-            return
-        }
-        
-        self.card = _card
-        
-        if let _actionOne = self.card.actionOne {
-            let title = NSAttributedString(string: _actionOne.title, attributes: ShadowAttributes.forLabelMedium)
-            actionOneButton.setAttributedTitle(title, for: UIControlState())
-            actionOneButton.isHidden = false
-        }
-        
-        if let _actionTwo = self.card.actionTwo {
-            let title = NSAttributedString(string: _actionTwo.title, attributes: ShadowAttributes.forLabelMedium)
-            actionTwoButton.setAttributedTitle(title, for: UIControlState())
-            actionTwoButton.isHidden = false
-        }
-        
-
-        imageView.loadImageAtURL(self.card.imageURLString, withDefaultImage: self.card.placeholderImage)
-        
-        titleLabel.attributedText = NSAttributedString(string: self.card.titleText, attributes: ShadowAttributes.forLabelSoft)
-        subTitleLabelOne.attributedText = NSAttributedString(string: self.card.detailTextLineOne, attributes: ShadowAttributes.forLabelSoft)
-        subTitleLabelTwo.attributedText = NSAttributedString(string: self.card.detailTextLineTwo, attributes: ShadowAttributes.forLabelSoft)
-    }
-
-    @IBAction func actionOneButtonAction(_ sender: AnyObject) {
-        delegate?.focusedCardViewDidSelectActionItemOne(self)
-    }
-    
-    @IBAction func actionTwoButtonAction(_ sender: AnyObject) {
-        delegate?.focusedCardViewDidSelectActionItemTwo(self)
-    }
-    
-    func tapAction() {
-        delegate?.focusedCardViewDidSelectDetailAction(self)
-    }
-}
-
-
